73a9babf1144fa0dc50770a022569ca2ad1f6752
Garrett Levy <garrettlr@gmail.com>
2017-09-04 20:51:39 -0700

    finshes style setup for login page

--- components/HomePage.js
+++ components/HomePage.js
@@ -12,24 +12,26 @@ export default class HomePage extends Component {
 
   render() {
     return (
-      <div className={`${styles.main}`}>
-        <div className={`${styles.login}`}>
-          <div id={`${styles.titlebox}`}>
+      <div className={styles.main}>
+        <div className={styles.login}>
+          <div id={styles.titlebox}>
             <img id={styles.title} src = "instagram-1.svg" />
           </div>
           <input
-            className={`${styles.input}`}
+            className={styles.input}
             placeholder="Username"
             type="text"
             >
             </input>
           <input
-            className={`${styles.input}`}
+            className={styles.input}
             placeholder="Password"
             type="password"
             >
             </input>
-
+          <div id={styles.loginButton}>
+            <Link to="/feed">Login</Link>
+          </div>
         </div>
       </div>
     );
