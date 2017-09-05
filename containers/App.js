f00f625948e669fac9596029063105714c53ef91
Garrett Levy <garrettlr@gmail.com>
2017-09-04 20:55:27 -0700

    removes unused file

--- containers/App.js
+++ containers/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import type { Children } from 'react';
-import styles from '../components/app.scss';
+
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className={styles.app}>
+      <div>
         {this.props.children}
       </div>
     );
