df8301f38af7172efa6c5a1f242f2838198f6a2d
Garrett Levy <garrettlr@gmail.com>
2017-09-13 14:02:36 -0700

    adds flow.

--- actions/loginActions.js
+++ actions/loginActions.js
@@ -3,30 +3,37 @@ export const CHECK_USER = 'CHECK_USER';
 export const GET_USER = 'GET_USER';
 export const GET_PASS = 'GET_PASS';
 
-const checkUser = () => ({ type: CHECK_USER });
-
-const getUser = payload => ({
+const getUser = (payload: string) => ({
   type: GET_USER,
   payload,
 });
 
-const getPass = payload => ({
+const getPass = (payload: string) => ({
   type: GET_PASS,
   payload,
 });
 
-export function createUsername(event) {
+export function checkUser() {
+  return { type: CHECK_USER };
+};
+
+export function createUsername(event: eventType) {
   return dispatch => {
     const username = event.target.value + event.key;
-    dispatch(getUser(username));
+    dispatch(getUser(username: string));
   };
 }
 
-export function createPassword(event) {
+export function createPassword(event: eventType) {
   return dispatch => {
     const password = event.target.value + event.key;
-    dispatch(getPass(password));
+    dispatch(getPass(password: string));
   };
 }
 
-export default checkUser;
+type eventType = {
+  target: {
+    value: string,
+  },
+  key: string,
+};
